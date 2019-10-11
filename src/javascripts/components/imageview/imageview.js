0961f43a898d98968f5b48027586eb640d800e7a
Maggie Greene <maggie.greene96@gmail.com>
2019-10-11 16:36:21 -0500

    Work in progress

---
+++ src/javascripts/components/imageview/imageview.js
@@ -0,0 +1,13 @@
+// import $ from 'jquery';
+// import utilities from '../../helpers/utilities';
+
+// const printImage = () => {
+//   const domString = '<p style="color: white">gday stranger</p>';
+//   utilities.printToDom('mainview', domString);
+// };
+
+// const attachEvents = () => {
+//   $(document).on('click', 'h5', printImage);
+// };
+
+// export default { attachEvents };
---
+++ src/javascripts/components/mainview/mainview.js
@@ -0,0 +1,36 @@
+import $ from 'jquery';
+import utilities from '../../helpers/utilities';
+import data from '../../helpers/data/planets';
+import './mainview.scss';
+
+const printMainCard = () => {
+  const planets = data.getPlanets();
+  let domString = '';
+  for (let i = 0; i < planets.length; i += 1) {
+    domString += `
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">${planets[i].name}</h5>
+      </div>
+    </div>
+    `;
+  }
+  utilities.printToDom('mainview', domString);
+};
+
+const printImage = () => {
+  const planetsTwo = data.getPlanets();
+  const domStringTwo = `    
+  <div class="card">
+    <div class="card-body">
+      <img class="card-title">${planetsTwo.imageUrl}</img>
+    </div>
+  </div>`;
+  utilities.printToDom('mainview', domStringTwo);
+};
+
+const attachEvents = () => {
+  $(document).on('click', 'h5', printImage);
+};
+
+export default { printMainCard, attachEvents };
