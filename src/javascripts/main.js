4a50b2422e195aa2608a1410021c45a1aabccfa0
Maggie Greene <maggie.greene96@gmail.com>
2019-10-28 18:10:27 -0500

    Fixed hide planet

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,14 +3,13 @@ import '../styles/main.scss';
 import mainview from './components/mainview/mainview';
 import imageview from './components/imageview/imageview';
 import soloplanet from './components/soloplanet/soloplanet';
-// import hide from './components/hideplanet/hideplanet';
 
 const init = () => {
   mainview.printMainCard();
   imageview.attachFirstEvent();
   imageview.attachEvent();
+  soloplanet.hidePlanet();
   soloplanet.showSoloPlanetView();
-  // soloplanet.hidePlanet();
 };
 
 init();
